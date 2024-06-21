3e2c8c578f11c261640992374fa36785ef752be7
Darwin-md11 <merchandarwin2@gmail.com>
2024-06-21 12:26:42 -0500

    Delete escuela directory

--- escuela/evaluacion.py
+++
@@ -1,8 +0,0 @@
-class Evaluacion:
-    def __init__(self, curso, estudiante, nota):
-        self.curso = curso
-        self.estudiante = estudiante
-        self.nota = nota
-
-    def mostrar_info(self):
-        return f"Curso: {self.curso}\nEstudiante: {self.estudiante}\nNota: {self.nota}"
--- escuela/horario.py
+++
@@ -1,8 +0,0 @@
-class Horario:
-    def __init__(self, dia, hora_inicio, hora_fin):
-        self.dia = dia
-        self.hora_inicio = hora_inicio
-        self.hora_fin = hora_fin
-
-    def mostrar_info(self):
-        return f"Día: {self.dia}\nHora de inicio: {self.hora_inicio}\nHora de fin: {self.hora_fin}"
